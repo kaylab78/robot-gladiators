47305b575d9c58bb69441213069d39c219de5c16
kaylab78 <kaylabackus78@gmail.com>
2022-05-11 17:09:52 -0500

    Added for statement and an array

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,6 +15,19 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+for(var i=0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -23,7 +36,7 @@ var fight = function() {
 
     //if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
-        //remove enemy's health by subtracting the amount set in the playerAttach variable
+        //remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
